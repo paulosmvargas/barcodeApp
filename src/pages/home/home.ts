f5b43d54c765c59c9d0340005ee554d489adc57b
Paulo Sergio Maciel Vargas <paulov@coopatos.local>
2019-06-25 17:20:52 -0300

    barcode

---
+++ src/pages/home/home.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BarcodeScannerOriginal, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  options: BarcodeScannerOptions;
+  encodText: string = '';
+  encodedData: any = {};
+  scannedData: any = {};
+  constructor(public navCtrl: NavController,
+              public scanner: BarcodeScannerOriginal) {
+
+  }
+
+  scan(){
+    this.options={
+      prompt: 'Scan ...'
+    };
+    this.scanner.scan(this.options).then((data) => {
+      this.scannedData = data;
+    }, (err) => {
+      console.log('Erro: ', err);
+    })
+  }
+
+  encode(){
+    this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodText).then((data) => {
+      this.encodedData = data
+    }, (err) => {
+      console.log('Erro: ', err)
+    })
+  }
+}
